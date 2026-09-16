8bb63b16f10077c4575edacf4e9a410e944a7c5c
liwliet021021021-coder <liwliet021021021@gmail.com>
2026-09-16 22:07:49 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+import os
 
 app = Flask(__name__)
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 @app.route("/")
 def home():
-    return "Rubika Support Bot Online"
+    webhook_url = "https://rubika-support-bot-production.up.railway.app/receiveUpdate"
+
+    r = requests.post(
+        f"https://botapi.rubika.ir/v3/{BOT_TOKEN}/updateBotEndpoints",
+        json={
+            "url": webhook_url,
+            "type": "ReceiveUpdate"
+        },
+        timeout=20
+    )
+
+    return {
+        "status": "ok",
+        "result": r.json()
+    }
 
 @app.route("/receiveUpdate", methods=["POST"])
-def update():
+def receive_update():
+    data = request.json
+
+    print("=" * 50)
+    print(data)
+    print("=" * 50)
+
     return {"ok": True}
 
 if __name__ == "__main__":
