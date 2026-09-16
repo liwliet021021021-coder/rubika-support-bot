3cde08ace90b1f3628687bf97a7b2e4e5314ec3e
liwliet021021021-coder <liwliet021021021@gmail.com>
2026-09-16 22:11:58 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,33 +6,35 @@ app = Flask(__name__)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-@app.route("/")
-def home():
-    webhook_url = "https://rubika-support-bot-production.up.railway.app/receiveUpdate"
+def send_message(chat_id, text):
+    url = f"https://botapi.rubika.ir/v3/{BOT_TOKEN}/sendMessage"
 
-    r = requests.post(
-        f"https://botapi.rubika.ir/v3/{BOT_TOKEN}/updateBotEndpoints",
+    requests.post(
+        url,
         json={
-            "url": webhook_url,
-            "type": "ReceiveUpdate"
+            "chat_id": chat_id,
+            "text": text
         },
         timeout=20
     )
-
-    return {
-        "status": "ok",
-        "result": r.json()
-    }
 
 @app.route("/receiveUpdate", methods=["POST"])
 def receive_update():
     data = request.json
 
-    print("=" * 50)
-    print(data)
-    print("=" * 50)
+    if data["update"]["type"] == "NewMessage":
+        chat_id = data["update"]["chat_id"]
+
+        send_message(
+            chat_id,
+            "✅ ربات آنلاین است."
+        )
 
     return {"ok": True}
+
+@app.route("/")
+def home():
+    return "ONLINE"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
